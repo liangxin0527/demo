13479bb8228e1e92b7b1b0e93152f386496e8b53
liangshucheng <liangshucheng@luvsec.com>
2021-04-16 14:36:19 +0800

    feat: 添加git模板文件测试
    添加git模板文件测试添加git模板文件测试添加git模板文件测试添加git模板文件测试添加git模板文件测试
    
    [skip ci]

---
+++ redis/src/main/java/com/lbaci/redis/TestGitTemplate.java
@@ -0,0 +1,15 @@
+package com.lbaci.redis;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author liang
+ * @date 2021/4/16
+ */
+@Slf4j
+public class TestGitTemplate {
+    public TestGitTemplate() {
+        log.info("test add git template");
+    }
+
+}
